b92b0f8f97e8280f3a342bd887d2a17da7cde480
lixinyang123 <lixinyangemil@outlook.com>
2024-04-02 02:57:35 +0800

    update exportor

---
+++ index.js
@@ -0,0 +1,12 @@
+import { App } from './lib/app.js'
+import { Middleware } from './lib/middleware.js'
+
+export {
+    App as Plankton,
+    Middleware
+}
+
+export default { 
+    Plankton: App,
+    Middleware
+}
